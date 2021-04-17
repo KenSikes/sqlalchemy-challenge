1c566a22eca001da84e924dff945da2ed6c2aa3c
Ken <kensikes988@gmail.com>
2021-04-17 13:32:43 -0700

    saving app routes and precip query

--- app.py
+++ app.py
@@ -8,5 +8,55 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
+#Database Setup
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+#Flask Setup
+app = Flask(__name__)
+
+#Flask Routes
+#Home page.
+#List all routes that are available.
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start (enter as YYYY-MM-DD)<br/>"
+        f"/api/v1.0/start/end (enter as YYYY-MM-DD/YYYY-MM-DD)"
+    )
+
+#/api/v1.0/precipitation
+#Convert the query results to a dictionary using date as the key and prcp as the value.
+#Return the JSON representation of your dictionary.
+@app.route("/api/v1.0/precipitation") 
+def precipitation():
+    results = (session.query(Measurement.date, Measurement.tobs)
+                      .order_by(Measurement.date))
+
+    precipitation_date_tobs = []
+    for each_row in results:
+        dt_dict = {}
+        dt_dict["date"] = each_row.date
+        dt_dict["tobs"] = each_row.tobs
+        precipitation_date_tobs.append(dt_dict)
+
+    return jsonify(precipitation_date_tobs)
 
 
